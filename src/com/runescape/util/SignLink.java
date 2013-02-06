a27bfbd26efdc57bbe2129782f5087f8ed166c5e
Ryan <Ryan@192.168.10.101>
2013-02-05 19:11:40 -0600

    Conflict

--- src/com/runescape/util/SignLink.java
+++ src/com/runescape/util/SignLink.java
@@ -15,59 +15,57 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.URL;
 
-public class SignLink implements Runnable {
+public class SignLink implements Runnable
+{
 	public static int uid;
-	public static int storeId = 32;
-	public static RandomAccessFile aRandomAccessFile613 = null;
-	public static RandomAccessFile[] aRandomAccessFileArray614 = new RandomAccessFile[5];
-	public static boolean aBoolean615;
+	public static int storeIndex = 32;
+	public static RandomAccessFile mainCache = null;
+	public static RandomAccessFile[] cacheIndexes = new RandomAccessFile[5];
 	public static Applet applet = null;
-	private static boolean running;
-	private static int anInt618;
-	private static InetAddress anInetAddress619;
-	private static int anInt620;
+	private static boolean active;
+	private static InetAddress nextInetAddress;
+	private static int nextPort;
 	private static Socket socket = null;
-	private static int anInt621 = 1;
-	private static Runnable aRunnable622 = null;
-	private static String aString623 = null;
-	public static String aString624 = null;
-	private static String aString625 = null;
-	private static DataInputStream aDataInputStream626 = null;
-	private static int anInt627;
-	private static String soundFileName = null;
-	private static byte[] aByteArray629 = null;
-	private static boolean aBoolean630;
-	private static int anInt631;
-	public static String aString632 = null;
-	public static int anInt633;
-	public static int anInt634;
-	private static boolean aBoolean635;
-	private static int anInt636;
-	public static String aString637 = null;
-	public static int anInt638;
-	public static boolean aBoolean639 = true;
-	public static String username = "";
+	private static int nextRunnablePriority = 1;
+	private static Runnable nextRunnable = null;
+	private static String lastIP = null;
+	public static String lastHostname = null;
+	private static String nextURL = null;
+	private static DataInputStream nextURLStream = null;
+	private static int nextWriteLength;
+	private static String nextWriteName = null;
+	private static byte[] nextWriteData = null;
+	private static boolean writeMusic;
+	private static int nextMidiIndex;
+	public static String nextSongName = null;
+	public static int midiVolume;
+	public static int midiFade;
+	private static boolean writeSoundEffect;
+	private static int nextWaveIndex;
+	public static String nextEffectName = null;
+	public static int waveVolume;
+	public static boolean accessible = true;
+	public static String lastUsername = "";
 
 	public static final void method547(InetAddress inetaddress) {
-		SignLink.anInt618 = (int) (Math.random() * 9.9999999E7);
-		if (SignLink.running) {
+		if (SignLink.active) {
 			try {
 				Thread.sleep(500L);
 			} catch (Exception exception) {
 				/* empty */
 			}
-			SignLink.running = false;
+			SignLink.active = false;
 		}
-		SignLink.anInt620 = 0;
-		SignLink.aRunnable622 = null;
-		SignLink.aString623 = null;
-		SignLink.soundFileName = null;
-		SignLink.aString625 = null;
-		SignLink.anInetAddress619 = inetaddress;
+		SignLink.nextPort = 0;
+		SignLink.nextRunnable = null;
+		SignLink.lastIP = null;
+		SignLink.nextWriteName = null;
+		SignLink.nextURL = null;
+		SignLink.nextInetAddress = inetaddress;
 		Thread thread = new Thread(new SignLink());
 		thread.setDaemon(true);
 		thread.start();
-		while (!SignLink.running) {
+		while (!SignLink.active) {
 			try {
 				Thread.sleep(50L);
 			} catch (Exception exception) {
@@ -78,72 +76,70 @@ public class SignLink implements Runnable {
 
 	@Override
 	public final void run() {
-		SignLink.running = true;
-		String cacheDirectory = SignLink.getCacheDirectory();
-		SignLink.uid = SignLink.method549(cacheDirectory);
+		SignLink.active = true;
+		String directory = SignLink.getCacheDirectory();
+		SignLink.uid = SignLink.getUID(directory);
+
 		try {
-			File file = new File(cacheDirectory + "main_file_cache.dat");
-			if (file.exists() && file.length() > 52428800L) {
+			File file = new File(directory + "main_file_cache.dat");
+			if (file.exists() && file.length() > 0x3200000) {
 				file.delete();
 			}
-			SignLink.aRandomAccessFile613 = new RandomAccessFile(cacheDirectory + "main_file_cache.dat", "rw");
+			SignLink.mainCache = new RandomAccessFile(directory + "main_file_cache.dat", "rw");
 			for (int i = 0; i < 5; i++) {
-				SignLink.aRandomAccessFileArray614[i] = new RandomAccessFile(
-						cacheDirectory + "main_file_cache.idx" + i, "rw");
+				SignLink.cacheIndexes[i] = new RandomAccessFile(directory + "main_file_cache.idx" + i, "rw");
 			}
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
-		int i = SignLink.anInt618;
-		while (SignLink.anInt618 == i) {
-			if (SignLink.anInt620 != 0) {
+
+		while (true) {
+			if (SignLink.nextPort != 0) {
 				try {
-					SignLink.socket = new Socket(SignLink.anInetAddress619, SignLink.anInt620);
+					SignLink.socket = new Socket(SignLink.nextInetAddress, SignLink.nextPort);
 				} catch (Exception exception) {
 					SignLink.socket = null;
 				}
-				SignLink.anInt620 = 0;
-			} else if (SignLink.aRunnable622 != null) {
-				Thread thread = new Thread(SignLink.aRunnable622);
+				SignLink.nextPort = 0;
+			} else if (SignLink.nextRunnable != null) {
+				Thread thread = new Thread(SignLink.nextRunnable);
 				thread.setDaemon(true);
 				thread.start();
-				thread.setPriority(SignLink.anInt621);
-				SignLink.aRunnable622 = null;
-			} else if (SignLink.aString623 != null) {
+				thread.setPriority(SignLink.nextRunnablePriority);
+				SignLink.nextRunnable = null;
+			} else if (SignLink.lastIP != null) {
 				try {
-					SignLink.aString624 = InetAddress.getByName(SignLink.aString623).getHostName();
+					SignLink.lastHostname = InetAddress.getByName(SignLink.lastIP).getHostName();
 				} catch (Exception exception) {
-					SignLink.aString624 = "unknown";
+					SignLink.lastHostname = "unknown";
 				}
-				SignLink.aString623 = null;
-			} else if (SignLink.soundFileName != null) {
-				if (SignLink.aByteArray629 != null) {
+				SignLink.lastIP = null;
+			} else if (SignLink.nextWriteName != null) {
+				if (SignLink.nextWriteData != null) {
 					try {
-						FileOutputStream fileoutputstream = new FileOutputStream(cacheDirectory
-								+ SignLink.soundFileName);
-						fileoutputstream.write(SignLink.aByteArray629, 0, SignLink.anInt627);
-						fileoutputstream.close();
+						FileOutputStream out = new FileOutputStream(directory + SignLink.nextWriteName);
+						out.write(SignLink.nextWriteData, 0, SignLink.nextWriteLength);
+						out.close();
 					} catch (Exception exception) {
 						/* empty */
 					}
 				}
-				if (SignLink.aBoolean635) {
-					SignLink.aString637 = cacheDirectory + SignLink.soundFileName;
-					SignLink.aBoolean635 = false;
+				if (SignLink.writeSoundEffect) {
+					SignLink.nextEffectName = directory + SignLink.nextWriteName;
+					SignLink.writeSoundEffect = false;
 				}
-				if (SignLink.aBoolean630) {
-					SignLink.aString632 = cacheDirectory + SignLink.soundFileName;
-					SignLink.aBoolean630 = false;
+				if (SignLink.writeMusic) {
+					SignLink.nextSongName = directory + SignLink.nextWriteName;
+					SignLink.writeMusic = false;
 				}
-				SignLink.soundFileName = null;
-			} else if (SignLink.aString625 != null) {
+				SignLink.nextWriteName = null;
+			} else if (SignLink.nextURL != null) {
 				try {
-					SignLink.aDataInputStream626 = new DataInputStream(new URL(SignLink.applet.getCodeBase(),
-							SignLink.aString625).openStream());
+					SignLink.nextURLStream = new DataInputStream(new URL(SignLink.applet.getCodeBase(), SignLink.nextURL).openStream());
 				} catch (Exception exception) {
-					SignLink.aDataInputStream626 = null;
+					SignLink.nextURLStream = null;
 				}
-				SignLink.aString625 = null;
+				SignLink.nextURL = null;
 			}
 			try {
 				Thread.sleep(50L);
@@ -157,30 +153,30 @@ public class SignLink implements Runnable {
 		return "./cache/";
 	}
 
-	private static final int method549(String string) {
+	private static final int getUID(String directory) {
 		try {
-			File file = new File(string + "uid.dat");
+			File file = new File(directory + "uid.dat");
 			if (!file.exists() || file.length() < 4L) {
-				DataOutputStream dataoutputstream = new DataOutputStream(new FileOutputStream(string + "uid.dat"));
-				dataoutputstream.writeInt((int) (Math.random() * 9.9999999E7));
-				dataoutputstream.close();
+				DataOutputStream out = new DataOutputStream(new FileOutputStream(directory + "uid.dat"));
+				out.writeInt((int) (Math.random() * 9.9999999E7));
+				out.close();
 			}
 		} catch (Exception exception) {
 			/* empty */
 		}
 		try {
-			DataInputStream datainputstream = new DataInputStream(new FileInputStream(string + "uid.dat"));
-			int i = datainputstream.readInt();
-			datainputstream.close();
+			DataInputStream in = new DataInputStream(new FileInputStream(directory + "uid.dat"));
+			int i = in.readInt();
+			in.close();
 			return i + 1;
 		} catch (Exception exception) {
 			return 0;
 		}
 	}
 
-	public static final synchronized Socket method550(int i) throws IOException {
-		SignLink.anInt620 = i;
-		while (SignLink.anInt620 != 0) {
+	public static final synchronized Socket openPort(int port) throws IOException {
+		SignLink.nextPort = port;
+		while (SignLink.nextPort != 0) {
 			try {
 				Thread.sleep(50L);
 			} catch (Exception exception) {
@@ -193,78 +189,77 @@ public class SignLink implements Runnable {
 		return SignLink.socket;
 	}
 
-	public static final synchronized DataInputStream openURL(String string) throws IOException {
-		SignLink.aString625 = string;
-		while (SignLink.aString625 != null) {
+	public static final synchronized DataInputStream getURLStream(String url) throws IOException {
+		SignLink.nextURL = url;
+		while (SignLink.nextURL != null) {
 			try {
 				Thread.sleep(50L);
 			} catch (Exception exception) {
 				/* empty */
 			}
 		}
-		if (SignLink.aDataInputStream626 == null) {
-			throw new IOException("could not open: " + string);
+		if (SignLink.nextURLStream == null) {
+			throw new IOException("could not open: " + url);
 		}
-		return SignLink.aDataInputStream626;
+		return SignLink.nextURLStream;
 	}
 
-	public static final synchronized void method552(String string) {
-		SignLink.aString624 = string;
-		SignLink.aString623 = string;
+	public static final synchronized void setLastIP(String string) {
+		SignLink.lastHostname = string;
+		SignLink.lastIP = string;
 	}
 
-	public static final synchronized void method553(Runnable runnable, int i) {
-		SignLink.anInt621 = i;
-		SignLink.aRunnable622 = runnable;
+	public static final synchronized void startRunnable(Runnable runnable, int priority) {
+		SignLink.nextRunnablePriority = priority;
+		SignLink.nextRunnable = runnable;
 	}
 
-	public static final synchronized boolean method554(byte[] bs, int i) {
-		if (i > 2000000) {
+	public static final synchronized boolean writeWave(byte[] data, int length) {
+		if (length > 0x1E8480) {
 			return false;
 		}
-		if (SignLink.soundFileName != null) {
+		if (SignLink.nextWriteName != null) {
 			return false;
 		}
-		SignLink.anInt636 = (SignLink.anInt636 + 1) % 5;
-		SignLink.anInt627 = i;
-		SignLink.aByteArray629 = bs;
-		SignLink.aBoolean635 = true;
-		SignLink.soundFileName = "sound" + SignLink.anInt636 + ".wav";
+		SignLink.nextWaveIndex = (SignLink.nextWaveIndex + 1) % 5;
+		SignLink.nextWriteLength = length;
+		SignLink.nextWriteData = data;
+		SignLink.writeSoundEffect = true;
+		SignLink.nextWriteName = "sound" + SignLink.nextWaveIndex + ".wav";
 		return true;
 	}
 
-	public static final synchronized boolean method555() {
-		if (SignLink.soundFileName != null) {
+	public static final synchronized boolean rewriteWave() {
+		if (SignLink.nextWriteName != null) {
 			return false;
 		}
-		SignLink.aByteArray629 = null;
-		SignLink.aBoolean635 = true;
-		SignLink.soundFileName = "sound" + SignLink.anInt636 + ".wav";
+		SignLink.nextWriteData = null;
+		SignLink.writeSoundEffect = true;
+		SignLink.nextWriteName = "sound" + SignLink.nextWaveIndex + ".wav";
 		return true;
 	}
 
-	public static final synchronized void method556(byte[] bs, int i) {
-		if (i <= 2000000 && SignLink.soundFileName == null) {
-			SignLink.anInt631 = (SignLink.anInt631 + 1) % 5;
-			SignLink.anInt627 = i;
-			SignLink.aByteArray629 = bs;
-			SignLink.aBoolean630 = true;
-			SignLink.soundFileName = "jingle" + SignLink.anInt631 + ".mid";
+	public static final synchronized void writeMidi(byte[] data, int i) {
+		if (i <= 0x1E8480 && SignLink.nextWriteName == null) {
+			SignLink.nextMidiIndex = (SignLink.nextMidiIndex + 1) % 5;
+			SignLink.nextWriteLength = i;
+			SignLink.nextWriteData = data;
+			SignLink.writeMusic = true;
+			SignLink.nextWriteName = "jingle" + SignLink.nextMidiIndex + ".mid";
 		}
 	}
 
-	public static final void reportError(String string) {
-		if (SignLink.aBoolean639 && SignLink.running) {
-			System.out.println("Error: " + string);
+	public static final void reportError(String error) {
+		if (SignLink.accessible && SignLink.active) {
+			System.out.println("Error: " + error);
 			try {
-				string = string.replace(':', '_');
-				string = string.replace('@', '_');
-				string = string.replace('&', '_');
-				string = string.replace('#', '_');
-				DataInputStream datainputstream = SignLink.openURL("reporterror" + 317 + ".cgi?error="
-						+ SignLink.username + " " + string);
-				datainputstream.readUTF();
-				datainputstream.close();
+				error = error.replace(':', '_');
+				error = error.replace('@', '_');
+				error = error.replace('&', '_');
+				error = error.replace('#', '_');
+				DataInputStream in = SignLink.getURLStream("reporterror" + 317 + ".cgi?error=" + SignLink.lastUsername + " " + error);
+				in.readUTF();
+				in.close();
 			} catch (IOException ioexception) {
 				/* empty */
 			}
