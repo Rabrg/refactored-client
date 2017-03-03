9cffb5b4d9e70a709b69f48b4bcc8a8ad79b8aef
Ryan Greene <rlgree1@ilstu.edu>
2017-03-03 15:28:25 -0600

    Readded com.jagex.runescape.cache

---
+++ src/com/jagex/runescape/cache/def/ActorDefinition.java
@@ -0,0 +1,292 @@
+package com.jagex.runescape.cache.def;
+
+import com.jagex.runescape.cache.Archive;
+import com.jagex.runescape.cache.cfg.VarBit;
+import com.jagex.runescape.collection.Cache;
+import com.jagex.runescape.media.Animation;
+import com.jagex.runescape.media.renderable.Model;
+import com.jagex.runescape.Game;
+import com.jagex.runescape.net.Buffer;
+
+public class ActorDefinition {
+
+	public int turnLeftAnimationId = -1;
+	private static int bufferIndex;
+	public int varBitId = -1;
+	public int turnAroundAnimationId = -1;
+	public int settingId = -1;
+	private static Buffer buffer;
+	public int combatLevel = -1;
+	public static int size;
+	public String name;
+	public String[] actions;
+	public int walkAnimationId = -1;
+	public byte boundaryDimension = 1;
+	private int[] originalModelColors;
+	private static int[] bufferOffsets;
+	private int[] headModelIndexes;
+	public int headIcon = -1;
+	private int[] modifiedModelColors;
+	public int standAnimationId = -1;
+	public long id = -1;
+	public int degreesToTurn = 32;
+	private static ActorDefinition[] cache;
+	public static Game client;
+	public int turnRightAnimationId = -1;
+	public boolean clickable = true;
+	private int brightness;
+	private int sizeY = 128;
+	public boolean minimapVisible = true;
+	public int[] childrenIds;
+	public byte[] description;
+	private int sizeXZ = 128;
+	private int contrast;
+	public boolean visible = false;
+	private int[] modelIds;
+	public static Cache modelCache = new Cache(30);
+
+	public static final ActorDefinition getDefinition(int id) {
+		for (int i = 0; i < 20; i++) {
+			if (ActorDefinition.cache[i].id == id) {
+				return ActorDefinition.cache[i];
+			}
+		}
+		ActorDefinition.bufferIndex = (ActorDefinition.bufferIndex + 1) % 20;
+		ActorDefinition definition = ActorDefinition.cache[ActorDefinition.bufferIndex] = new ActorDefinition();
+		ActorDefinition.buffer.offset = ActorDefinition.bufferOffsets[id];
+		definition.id = id;
+		definition.loadDefinition(true, ActorDefinition.buffer);
+		return definition;
+	}
+
+	public final Model getHeadModel() {
+		if (childrenIds != null) {
+			ActorDefinition definition = getChildDefinition();
+			if (definition == null) {
+				return null;
+			}
+			return definition.getHeadModel();
+		}
+		if (headModelIndexes == null) {
+			return null;
+		}
+		boolean cached = false;
+		for (int headModel = 0; headModel < headModelIndexes.length; headModel++) {
+			if (!Model.isCached(headModelIndexes[headModel])) {
+				cached = true;
+			}
+		}
+		if (cached) {
+			return null;
+		}
+		Model[] headModels = new Model[headModelIndexes.length];
+		for (int model = 0; model < headModelIndexes.length; model++) {
+			headModels[model] = Model.getModel(headModelIndexes[model]);
+		}
+		Model headModel;
+		if (headModels.length == 1) {
+			headModel = headModels[0];
+		} else {
+			headModel = new Model(headModels.length, headModels);
+		}
+		if (modifiedModelColors != null) {
+			for (int color = 0; color < modifiedModelColors.length; color++) {
+				headModel.recolor(modifiedModelColors[color], originalModelColors[color]);
+			}
+		}
+		return headModel;
+	}
+
+	public final ActorDefinition getChildDefinition() {
+		int childId = -1;
+		if (varBitId != -1) {
+			VarBit varbit = VarBit.cache[varBitId];
+			int configId = varbit.configId;
+			int leastSignificantBit = varbit.leastSignificantBit;
+			int mostSignificantBit = varbit.mostSignificantBit;
+			int bit = Game.BITFIELD_MAX_VALUE[mostSignificantBit - leastSignificantBit];
+			childId = ActorDefinition.client.widgetSettings[configId] >> leastSignificantBit & bit;
+		} else if (settingId != -1) {
+			childId = ActorDefinition.client.widgetSettings[settingId];
+		}
+		if (childId < 0 || childId >= childrenIds.length || childrenIds[childId] == -1) {
+			return null;
+		}
+		return ActorDefinition.getDefinition(childrenIds[childId]);
+	}
+
+	public static final void load(Archive archive) {
+		ActorDefinition.buffer = new Buffer(archive.getFile("npc.dat"));
+		Buffer buffer = new Buffer(archive.getFile("npc.idx"));
+		ActorDefinition.size = buffer.getUnsignedLEShort();
+		ActorDefinition.bufferOffsets = new int[ActorDefinition.size];
+		int offset = 2;
+		for (int bufferIndex = 0; bufferIndex < ActorDefinition.size; bufferIndex++) {
+			ActorDefinition.bufferOffsets[bufferIndex] = offset;
+			offset += buffer.getUnsignedLEShort();
+		}
+		ActorDefinition.cache = new ActorDefinition[20];
+		for (int cacheIndex = 0; cacheIndex < 20; cacheIndex++) {
+			ActorDefinition.cache[cacheIndex] = new ActorDefinition();
+		}
+	}
+
+	public static final void reset() {
+		ActorDefinition.modelCache = null;
+		ActorDefinition.bufferOffsets = null;
+		ActorDefinition.cache = null;
+		ActorDefinition.buffer = null;
+	}
+
+	public final Model getChildModel(int frameId2, int frameId, int[] framesFrom2) {
+		if (childrenIds != null) {
+			ActorDefinition childDefinition = getChildDefinition();
+			if (childDefinition == null) {
+				return null;
+			}
+			return childDefinition.getChildModel(frameId2, frameId, framesFrom2);
+		}
+		Model childIdModel = (Model) ActorDefinition.modelCache.get(id);
+		if (childIdModel == null) {
+			boolean cached = false;
+			for (int modelId : modelIds) {
+				if (Model.isCached(modelId)) {
+					cached = true;
+				}
+			}
+			if (!cached) {
+				return null;
+			}
+			Model[] childModels = new Model[modelIds.length];
+			for (int model = 0; model < modelIds.length; model++) {
+				childModels[model] = Model.getModel(modelIds[model]);
+			}
+			if (childModels.length == 1) {
+				childIdModel = childModels[0];
+			} else {
+				childIdModel = new Model(childModels.length, childModels);
+			}
+			if (modifiedModelColors != null) {
+				for (int color = 0; color < modifiedModelColors.length; color++) {
+					childIdModel.recolor(modifiedModelColors[color], originalModelColors[color]);
+				}
+			}
+			childIdModel.createBones();
+			childIdModel.applyLighting(64 + brightness, 850 + contrast, -30, -50, -30, true);
+			ActorDefinition.modelCache.put(childIdModel, id);
+		}
+		Model childModel = Model.aModel1614;
+		childModel.replaceWithModel(childIdModel, Animation.exists(frameId) & Animation.exists(frameId2));
+		if (frameId != -1 && frameId2 != -1) {
+			childModel.mixAnimationFrames(-20491, framesFrom2, frameId2, frameId);
+		} else if (frameId != -1) {
+			childModel.applyTransform(frameId);
+		}
+		if (sizeXZ != 128 || sizeY != 128) {
+			childModel.scaleT(sizeXZ, sizeXZ, sizeY);
+		}
+		childModel.calculateDiagonals();
+		childModel.triangleSkin = null;
+		childModel.vectorSkin = null;
+		if (boundaryDimension == 1) {
+			childModel.oneSquareModel = true;
+		}
+		return childModel;
+	}
+
+	private final void loadDefinition(boolean bool, Buffer buffer) {
+		while (true) {
+			int attributeId = buffer.getUnsignedByte();
+			if (attributeId == 0) {
+				break;
+			}
+			if (attributeId == 1) {
+				int modelCount = buffer.getUnsignedByte();
+				modelIds = new int[modelCount];
+				for (int model = 0; model < modelCount; model++) {
+					modelIds[model] = buffer.getUnsignedLEShort();
+				}
+			} else if (attributeId == 2) {
+				name = buffer.getString();
+			} else if (attributeId == 3) {
+				description = buffer.getStringAsBytes();
+			} else if (attributeId == 12) {
+				boundaryDimension = buffer.get();
+			} else if (attributeId == 13) {
+				standAnimationId = buffer.getUnsignedLEShort();
+			} else if (attributeId == 14) {
+				walkAnimationId = buffer.getUnsignedLEShort();
+			} else if (attributeId == 17) {
+				walkAnimationId = buffer.getUnsignedLEShort();
+				turnAroundAnimationId = buffer.getUnsignedLEShort();
+				turnRightAnimationId = buffer.getUnsignedLEShort();
+				turnLeftAnimationId = buffer.getUnsignedLEShort();
+			} else if (attributeId >= 30 && attributeId < 40) {
+				if (actions == null) {
+					actions = new String[5];
+				}
+				actions[attributeId - 30] = buffer.getString();
+				if (actions[attributeId - 30].equalsIgnoreCase("hidden")) {
+					actions[attributeId - 30] = null;
+				}
+			} else if (attributeId == 40) {
+				int modelColorCount = buffer.getUnsignedByte();
+				modifiedModelColors = new int[modelColorCount];
+				originalModelColors = new int[modelColorCount];
+				for (int color = 0; color < modelColorCount; color++) {
+					modifiedModelColors[color] = buffer.getUnsignedLEShort();
+					originalModelColors[color] = buffer.getUnsignedLEShort();
+				}
+			} else if (attributeId == 60) {
+				int additionalModelCount = buffer.getUnsignedByte();
+				headModelIndexes = new int[additionalModelCount];
+				for (int model = 0; model < additionalModelCount; model++) {
+					headModelIndexes[model] = buffer.getUnsignedLEShort();
+				}
+			} else if (attributeId == 90) {
+				buffer.getUnsignedLEShort(); // dummy
+			} else if (attributeId == 91) {
+				buffer.getUnsignedLEShort(); // dummy
+			} else if (attributeId == 92) {
+				buffer.getUnsignedLEShort(); // dummy
+			} else if (attributeId == 93) {
+				minimapVisible = false;
+			} else if (attributeId == 95) {
+				combatLevel = buffer.getUnsignedLEShort();
+			} else if (attributeId == 97) {
+				sizeXZ = buffer.getUnsignedLEShort();
+			} else if (attributeId == 98) {
+				sizeY = buffer.getUnsignedLEShort();
+			} else if (attributeId == 99) {
+				visible = true;
+			} else if (attributeId == 100) {
+				brightness = buffer.get();
+			} else if (attributeId == 101) {
+				contrast = buffer.get() * 5;
+			} else if (attributeId == 102) {
+				headIcon = buffer.getUnsignedLEShort();
+			} else if (attributeId == 103) {
+				degreesToTurn = buffer.getUnsignedLEShort();
+			} else if (attributeId == 106) {
+				varBitId = buffer.getUnsignedLEShort();
+				if (varBitId == 65535) {
+					varBitId = -1;
+				}
+				settingId = buffer.getUnsignedLEShort();
+				if (settingId == 65535) {
+					settingId = -1;
+				}
+				int childrenCount = buffer.getUnsignedByte();
+				childrenIds = new int[childrenCount + 1];
+				for (int child = 0; child <= childrenCount; child++) {
+					childrenIds[child] = buffer.getUnsignedLEShort();
+					if (childrenIds[child] == 65535) {
+						childrenIds[child] = -1;
+					}
+				}
+			} else if (attributeId == 107) {
+				clickable = false;
+			}
+		}
+	}
+}
---
+++ src/com/jagex/runescape/cache/def/FloorDefinition.java
@@ -0,0 +1,166 @@
+package com.jagex.runescape.cache.def;
+
+import com.jagex.runescape.cache.Archive;
+import com.jagex.runescape.net.Buffer;
+
+public class FloorDefinition {
+
+	public static int count;
+	public static FloorDefinition[] cache;
+	public String name;
+	public int rgbColor;
+	public int textureId = -1;
+	public boolean aBoolean227 = false;
+	public boolean occlude = true;
+	public int hue2;
+	public int saturation;
+	public int lightness;
+	public int hue;
+	public int hueDivisor;
+	public int hslColor2;
+
+	public static void load(Archive archive) {
+		Buffer buffer = new Buffer(archive.getFile("flo.dat"));
+		FloorDefinition.count = buffer.getUnsignedLEShort();
+		if (FloorDefinition.cache == null) {
+			FloorDefinition.cache = new FloorDefinition[FloorDefinition.count];
+		}
+		for (int floor = 0; floor < FloorDefinition.count; floor++) {
+			if (FloorDefinition.cache[floor] == null) {
+				FloorDefinition.cache[floor] = new FloorDefinition();
+			}
+			FloorDefinition.cache[floor].loadDefinition(true, buffer);
+		}
+	}
+
+	public void loadDefinition(boolean bool, Buffer buffer) {
+		while (true) {
+			int attributeId = buffer.getUnsignedByte();
+			if (attributeId == 0) {
+				break;
+			}
+			if (attributeId == 1) {
+				rgbColor = buffer.get24BitInt();
+				shiftRGBColors(rgbColor);
+			} else if (attributeId == 2) {
+				textureId = buffer.getUnsignedByte();
+			} else if (attributeId == 3) {
+				aBoolean227 = true;
+			} else if (attributeId == 5) {
+				occlude = false;
+			} else if (attributeId == 6) {
+				name = buffer.getString();
+			} else if (attributeId == 7) {
+				int oldHue2 = hue2;
+				int oldSaturation = saturation;
+				int oldLightness = lightness;
+				int oldHue = hue;
+				shiftRGBColors(buffer.get24BitInt());
+				hue2 = oldHue2;
+				saturation = oldSaturation;
+				lightness = oldLightness;
+				hue = oldHue;
+				hueDivisor = oldHue;
+			} else {
+				System.out.println("Error unrecognised config code: " + attributeId);
+			}
+		}
+	}
+
+	private void shiftRGBColors(int color) {
+		double r = (color >> 16 & 0xff) / 256.0;
+		double b = (color >> 8 & 0xff) / 256.0;
+		double g = (color & 0xff) / 256.0;
+		double cmin = r;
+		if (b < cmin) {
+			cmin = b;
+		}
+		if (g < cmin) {
+			cmin = g;
+		}
+		double cmax = r;
+		if (b > cmax) {
+			cmax = b;
+		}
+		if (g > cmax) {
+			cmax = g;
+		}
+		double d_11_ = 0.0;
+		double d_12_ = 0.0;
+		double d_13_ = (cmin + cmax) / 2.0;
+		if (cmin != cmax) {
+			if (d_13_ < 0.5) {
+				d_12_ = (cmax - cmin) / (cmax + cmin);
+			}
+			if (d_13_ >= 0.5) {
+				d_12_ = (cmax - cmin) / (2.0 - cmax - cmin);
+			}
+			if (r == cmax) {
+				d_11_ = (b - g) / (cmax - cmin);
+			} else if (b == cmax) {
+				d_11_ = 2.0 + (g - r) / (cmax - cmin);
+			} else if (g == cmax) {
+				d_11_ = 4.0 + (r - b) / (cmax - cmin);
+			}
+		}
+		d_11_ /= 6.0;
+		hue2 = (int) (d_11_ * 256.0);
+		saturation = (int) (d_12_ * 256.0);
+		lightness = (int) (d_13_ * 256.0);
+		if (saturation < 0) {
+			saturation = 0;
+		} else if (saturation > 255) {
+			saturation = 255;
+		}
+		if (lightness < 0) {
+			lightness = 0;
+		} else if (lightness > 255) {
+			lightness = 255;
+		}
+		if (d_13_ > 0.5) {
+			hueDivisor = (int) ((1.0 - d_13_) * d_12_ * 512.0);
+		} else {
+			hueDivisor = (int) (d_13_ * d_12_ * 512.0);
+		}
+		if (hueDivisor < 1) {
+			hueDivisor = 1;
+		}
+		hue = (int) (d_11_ * hueDivisor);
+		int huerand = hue2 + (int) (Math.random() * 16.0) - 8;
+		if (huerand < 0) {
+			huerand = 0;
+		} else if (huerand > 255) {
+			huerand = 255;
+		}
+		int satrand = saturation + (int) (Math.random() * 48.0) - 24;
+		if (satrand < 0) {
+			satrand = 0;
+		} else if (satrand > 255) {
+			satrand = 255;
+		}
+		int lightrand = lightness + (int) (Math.random() * 48.0) - 24;
+		if (lightrand < 0) {
+			lightrand = 0;
+		} else if (lightrand > 255) {
+			lightrand = 255;
+		}
+		hslColor2 = shiftHSLColors(huerand, satrand, lightrand);
+	}
+
+	private final int shiftHSLColors(int i, int i_17_, int i_18_) {
+		if (i_18_ > 179) {
+			i_17_ /= 2;
+		}
+		if (i_18_ > 192) {
+			i_17_ /= 2;
+		}
+		if (i_18_ > 217) {
+			i_17_ /= 2;
+		}
+		if (i_18_ > 243) {
+			i_17_ /= 2;
+		}
+		int i_19_ = (i / 4 << 10) + (i_17_ / 32 << 7) + i_18_ / 2;
+		return i_19_;
+	}
+}
